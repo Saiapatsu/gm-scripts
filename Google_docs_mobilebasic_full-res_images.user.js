1b6aef2ee94acadd3501764ccd1d15c32ca8bc56
Saiapatsu <26184960+Saiapatsu@users.noreply.github.com>
2023-11-11 03:28:00 +0200

    Cut down the double-googled links

--- Google_docs_mobilebasic_full-res_images.user.js
+++ Google_docs_mobilebasic_full-res_images.user.js
@@ -6,4 +6,13 @@
 // @grant       none
 // ==/UserScript==
 
-Array.from(document.getElementsByTagName("img")).forEach(node=>node.src = node.src.replace(/=s800$/, "=s0"));
+Array.forEach(document.getElementsByTagName("img"), node => {
+	node.src = node.src.replace(/=s800$/, "=s0");
+});
+
+Array.forEach(document.getElementsByTagName("a"), a => {
+	const url = new URL(a.href);
+	if (!url.searchParams.get("q")) return;
+	const url2 = new URL(url.searchParams.get("q"));
+	a.href = url2.searchParams.get("q");
+});
