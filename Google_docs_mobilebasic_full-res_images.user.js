51b990d80669d412e45300b8fc042ad79a419b6e
Saiapatsu <26184960+Saiapatsu@users.noreply.github.com>
2025-03-14 09:26:27 +0200

    Fix link fixer

--- Google_docs_mobilebasic_full-res_images.user.js
+++ Google_docs_mobilebasic_full-res_images.user.js
@@ -7,10 +7,11 @@
 // ==/UserScript==
 
 Array.forEach(document.getElementsByTagName("img"), node => {
-	node.src = node.src.replace(/=s800$/, "=s0");
+	node.src = node.src.replace(/=s800/, "=s0");
 });
 
 Array.forEach(document.getElementsByTagName("a"), a => {
+	if (!a.href) return;
 	const url = new URL(a.href);
 	const q = url.searchParams.get("q");
 	if (!q) return;
