6a8dd38ec5263c3eb5d2cd0483b6c4dd76480418
Saiapatsu <26184960+Saiapatsu@users.noreply.github.com>
2025-09-19 03:04:08 +0300

    Check-in old uncommitted changes

--- Google_docs_mobilebasic_full-res_images.user.js
+++ Google_docs_mobilebasic_full-res_images.user.js
@@ -1,15 +1,18 @@
 // ==UserScript==
 // @name        Google docs mobilebasic full-res images
 // @namespace   Wist
-// @include     http*://docs.google.com/document/d/*/mobilebasic*
+// @include     https://docs.google.com/document/d/*/mobilebasic*
 // @version     1
 // @grant       none
 // ==/UserScript==
 
+// Un-thumbnail every image (fragile, may break when Google changes the
+// max size of a thumbnail from 800 to something else)
 Array.forEach(document.getElementsByTagName("img"), node => {
 	node.src = node.src.replace(/=s800/, "=s0");
 });
 
+// Strip Google redirector from links
 Array.forEach(document.getElementsByTagName("a"), a => {
 	if (!a.href) return;
 	const url = new URL(a.href);
@@ -17,3 +20,6 @@ Array.forEach(document.getElementsByTagName("a"), a => {
 	if (!q) return;
 	a.href = new URL(q).searchParams.get("q");
 });
+
+// Remove app-container's scrollbar
+document.getElementsByClassName("app-container")[0].style.overflow = "unset";
