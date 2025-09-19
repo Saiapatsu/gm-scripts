1ab26cd5acd6953d5f5e14f233c3539c3ec2c011
Saiapatsu <26184960+Saiapatsu@users.noreply.github.com>
2025-09-19 03:17:07 +0300

    Add mobilebasic redirector

---
+++ Google_Docs_mobilebasic.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        Google Docs mobilebasic
+// @namespace   Wist
+// @include     http*://docs.google.com/document/d/*
+// @version     1
+// @grant       none
+// @run-at      document-start
+// ==/UserScript==
+
+;(()=>{
+	if (document.location.pathname.match(/[^\/]*$/)[0] !== "mobilebasic") {
+		
+		// Redirect to mobilebasic mode
+		window.stop();
+		document.location.pathname = document.location.pathname.replace(/[^\/]*$/, "mobilebasic");
+		
+	}
+})();
